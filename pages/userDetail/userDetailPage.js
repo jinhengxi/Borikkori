3358f9c5b3dc1c66f3cecca16b09e550863f2121
camilleson <asa.son59@gamil.com>
2022-08-20 21:01:35 +0900

    브랜치 생성

---
+++ pages/userDetail/userDetailPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const userDetailPage = () => {
+  return (
+    <Firtsdiv>userDetailPage</Firtsdiv>
+  )
+}
+const Firtsdiv = styled.div`
+height: 138.5px;
+border:1px solid red ;
+`;
+
+export default userDetailPage
