9c187f6bb3a96543bd53e73274edd2d030ac1f02
Hyeong kyeom Kim <78855917+Kyeom1997@users.noreply.github.com>
2022-08-20 16:24:56 +0900

    ADD: Navbar 레이아웃 1차 완료 Modify: Navbar Component 폴더로 이동

---
+++ components/main/BestCarousel.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+
+import BestCarouselCard from './BestCarouselCard';
+
+function BestCarousel() {
+  const [bestRecipes, setBestRecipes] = useState();
+
+  useEffect(() => {
+    fetch('data/DATA.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setBestRecipes(data);
+      });
+  }, []);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
+  return (
+    <StyledSlider {...settings}>
+      {bestRecipes?.map(bestRecipe => (
+        <BestCard key={bestRecipe}>
+          <BestCarouselCard bestRecipe={bestRecipe} />
+        </BestCard>
+      ))}
+    </StyledSlider>
+  );
+}
+
+export default BestCarousel;
+
+const StyledSlider = styled(Slider)`
+  .slick-list {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    overflow-x: hidden;
+  }
+
+  .slick-slide div {
+    outline: none;
+  }
+
+  .slick-arrow {
+    &::before {
+      font-size: 50px;
+    }
+  }
+
+  .slick-prev {
+    top: 45%;
+    left: 40px;
+    z-index: 1;
+  }
+
+  .slick-next {
+    top: 45%;
+    right: 40px;
+    z-index: 1;
+  }
+`;
+
+const BestCard = styled.div`
+  width: 223px;
+  height: 307px;
+  margin: 0 13px;
+  cursor: pointer;
+`;
