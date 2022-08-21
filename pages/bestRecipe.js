b359f56cc6e2c64d495207f05f313868f7d5c96d
Hyeong kyeom Kim <78855917+Kyeom1997@users.noreply.github.com>
2022-08-21 21:22:07 +0900

    Best Recipe 레이아웃 1차 구현

--- pages/bestRecipe.js
+++ pages/bestRecipe.js
@@ -3,26 +3,161 @@ import styled from 'styled-components';
 
 const bestRecipe = () => {
   return (
-    <>
-      <BestWrapper>
-        <BestTitle>BEST</BestTitle>
-        <BestImgWrapper>
-          <BestImgDiv></BestImgDiv>
-          <BestImgDiv>
-            <BestImgRightFirstDiv></BestImgRightFirstDiv>
-            <BestImgRightSecondDiv></BestImgRightSecondDiv>
-          </BestImgDiv>
-          <BestImgDiv>
-            <BestImgLeftFirstDiv>
-              <BestImgLeftFirstFirstDiv></BestImgLeftFirstFirstDiv>
-              <BestImgLeftFirstSecondDiv></BestImgLeftFirstSecondDiv>
-            </BestImgLeftFirstDiv>
-            <BestImgLeftSecondDiv></BestImgLeftSecondDiv>
-          </BestImgDiv>
-          <BestImgDiv></BestImgDiv>
-        </BestImgWrapper>
-      </BestWrapper>
-    </>
+    <BestWrapper>
+      <BestTitle>BEST</BestTitle>
+      <BestImgWrapper>
+        <BestImgDiv>
+          <BestInfoDiv>
+            <BestIconsWrapper>
+              <Views>
+                <InfoIcons src="/assets/images/BestViews.png" />
+                <span>1021</span>
+              </Views>
+              <Likes>
+                <InfoIcons src="/assets/images/BestLikes.png" />
+                <span>1512</span>
+              </Likes>
+              <Comments>
+                <CommentsIcon src="/assets/images/BestComments.png" />
+                <span>1313</span>
+              </Comments>
+            </BestIconsWrapper>
+            <UserInfo>
+              <UserIcon src="assets/images/userIcon.png" />
+              <UserRank src="assets/images/userRank.png" />
+              <UserName>tokkichef</UserName>
+            </UserInfo>
+            <BestName>요리제목</BestName>
+            <BestDesc>한줄설명한줄설명한줄설명한줄설명</BestDesc>
+          </BestInfoDiv>
+        </BestImgDiv>
+        <BestImgDiv>
+          <BestInfoDiv>
+            <BestIconsWrapper>
+              <Views>
+                <InfoIcons src="/assets/images/BestViews.png" />
+                <span>1021</span>
+              </Views>
+              <Likes>
+                <InfoIcons src="/assets/images/BestLikes.png" />
+                <span>1512</span>
+              </Likes>
+              <Comments>
+                <CommentsIcon src="/assets/images/BestComments.png" />
+                <span>1313</span>
+              </Comments>
+            </BestIconsWrapper>
+            <UserInfo>
+              <UserIcon src="assets/images/userIcon.png" />
+              <UserRank src="assets/images/userRank.png" />
+              <UserName>tokkichef</UserName>
+            </UserInfo>
+            <BestName>요리제목</BestName>
+            <BestDesc>한줄설명한줄설명한줄설명한줄설명</BestDesc>
+          </BestInfoDiv>
+        </BestImgDiv>
+        <BestImgDiv>
+          <BestInfoDiv>
+            <BestIconsWrapper>
+              <Views>
+                <InfoIcons src="/assets/images/BestViews.png" />
+                <span>1021</span>
+              </Views>
+              <Likes>
+                <InfoIcons src="/assets/images/BestLikes.png" />
+                <span>1512</span>
+              </Likes>
+              <Comments>
+                <CommentsIcon src="/assets/images/BestComments.png" />
+                <span>1313</span>
+              </Comments>
+            </BestIconsWrapper>
+            <UserInfo>
+              <UserIcon src="assets/images/userIcon.png" />
+              <UserRank src="assets/images/userRank.png" />
+              <UserName>tokkichef</UserName>
+            </UserInfo>
+            <BestName>요리제목</BestName>
+            <BestDesc>한줄설명한줄설명한줄설명한줄설명</BestDesc>
+          </BestInfoDiv>
+        </BestImgDiv>
+        <BestImgDiv>
+          <BestInfoDiv>
+            <BestIconsWrapper>
+              <Views>
+                <InfoIcons src="/assets/images/BestViews.png" />
+                <span>1021</span>
+              </Views>
+              <Likes>
+                <InfoIcons src="/assets/images/BestLikes.png" />
+                <span>1512</span>
+              </Likes>
+              <Comments>
+                <CommentsIcon src="/assets/images/BestComments.png" />
+                <span>1313</span>
+              </Comments>
+            </BestIconsWrapper>
+            <UserInfo>
+              <UserIcon src="assets/images/userIcon.png" />
+              <UserRank src="assets/images/userRank.png" />
+              <UserName>tokkichef</UserName>
+            </UserInfo>
+            <BestName>요리제목</BestName>
+            <BestDesc>한줄설명한줄설명한줄설명한줄설명</BestDesc>
+          </BestInfoDiv>
+        </BestImgDiv>
+        <BestImgDiv>
+          <BestInfoDiv>
+            <BestIconsWrapper>
+              <Views>
+                <InfoIcons src="/assets/images/BestViews.png" />
+                <span>1021</span>
+              </Views>
+              <Likes>
+                <InfoIcons src="/assets/images/BestLikes.png" />
+                <span>1512</span>
+              </Likes>
+              <Comments>
+                <CommentsIcon src="/assets/images/BestComments.png" />
+                <span>1313</span>
+              </Comments>
+            </BestIconsWrapper>
+            <UserInfo>
+              <UserIcon src="assets/images/userIcon.png" />
+              <UserRank src="assets/images/userRank.png" />
+              <UserName>tokkichef</UserName>
+            </UserInfo>
+            <BestName>요리제목</BestName>
+            <BestDesc>한줄설명한줄설명한줄설명한줄설명</BestDesc>
+          </BestInfoDiv>
+        </BestImgDiv>
+        <BestImgDiv>
+          <BestInfoDiv>
+            <BestIconsWrapper>
+              <Views>
+                <InfoIcons src="/assets/images/BestViews.png" />
+                <span>1021</span>
+              </Views>
+              <Likes>
+                <InfoIcons src="/assets/images/BestLikes.png" />
+                <span>1512</span>
+              </Likes>
+              <Comments>
+                <CommentsIcon src="/assets/images/BestComments.png" />
+                <span>1313</span>
+              </Comments>
+            </BestIconsWrapper>
+            <UserInfo>
+              <UserIcon src="assets/images/userIcon.png" />
+              <UserRank src="assets/images/userRank.png" />
+              <UserName>tokkichef</UserName>
+            </UserInfo>
+            <BestName>요리제목</BestName>
+            <BestDesc>한줄설명한줄설명한줄설명한줄설명</BestDesc>
+          </BestInfoDiv>
+        </BestImgDiv>
+      </BestImgWrapper>
+    </BestWrapper>
   );
 };
 
@@ -47,49 +182,134 @@ const BestImgWrapper = styled.div`
   flex-wrap: wrap;
 `;
 
+const BestName = styled.p`
+  margin: 10px 0;
+  width: 174px;
+  height: 28px;
+  font-size: 30px;
+  font-weight: bold;
+  color: #d9d9d9;
+`;
+
+const BestDesc = styled.p`
+  width: 470px;
+  height: 41px;
+  font-size: 18px;
+  color: #d9d9d9;
+`;
+
 const BestImgDiv = styled.div`
   width: 470px;
   height: 470px;
   margin: 10px;
-  border: 1px solid black;
-`;
-
-const BestImgRightFirstDiv = styled.div`
-  width: 470px;
-  height: 225px;
-  margin-bottom: 20px;
-  border: 1px solid black;
-`;
-
-const BestImgRightSecondDiv = styled.div`
-  width: 470px;
-  height: 225px;
-  border: 1px solid black;
-`;
-
-const BestImgLeftFirstDiv = styled.div`
-  width: 470px;
-  height: 225px;
-  margin-bottom: 20px;
-  border: 1px solid black;
   display: flex;
+  cursor: pointer;
+  background-size: 200%;
+  background-image: url('images/음식.jpg');
+  background-position: center;
+  transition: background-size 0.3s ease;
+  -moz-transition: background-size 0.3s ease;
+  -web-kit-transition: background-size 0.3s ease;
+  &:hover ${BestName} {
+    text-decoration: underline;
+    color: white;
+  }
+
+  &:hover ${BestDesc} {
+    color: white;
+  }
+
+  &:hover {
+    background-size: 220%;
+  }
 `;
 
-const BestImgLeftFirstFirstDiv = styled.div`
-  width: 225px;
-  height: 225px;
-  margin-right: 20px;
-  border: 1px solid black;
-`;
-
-const BestImgLeftFirstSecondDiv = styled.div`
-  width: 225px;
-  height: 225px;
-  border: 1px solid black;
-`;
-
-const BestImgLeftSecondDiv = styled.div`
+const BestInfoDiv = styled.div`
+  padding: 50px 40px;
+  height: 470px;
   width: 470px;
-  height: 225px;
-  border: 1px solid black;
+  display: flex;
+  background-color: rgba(0, 0, 0, 0.6);
+  justify-content: space-between;
+  flex-direction: column;
+  transition: 0.5s;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 10px 0;
+`;
+
+const UserIcon = styled.img`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin-right: 5px;
+`;
+
+const UserRank = styled.img`
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  margin-right: 5px;
+`;
+
+const UserName = styled.span`
+  width: 60px;
+  height: 11px;
+  font-size: 14px;
+  color: white;
+`;
+
+const BestIconsWrapper = styled.div`
+  width: 235px;
+  height: 470px;
+  display: flex;
+  font-weight: 200;
+  font-size: 20px;
+  color: white;
+`;
+
+const InfoIcons = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
+`;
+
+const CommentsIcon = styled.img`
+  width: 30px;
+  height: 20px;
+  margin-right: 5px;
+`;
+
+const Views = styled.div`
+  width: 80px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 8px;
+`;
+
+const Likes = styled.div`
+  width: 80px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 8px;
+`;
+
+const Comments = styled.div`
+  width: 80px;
+  height: 40px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  margin-right: 8px;
 `;
