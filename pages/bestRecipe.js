ec2a6c9e67f2d5a66971db40b8d33343cfbea342
Hyeong kyeom Kim <78855917+Kyeom1997@users.noreply.github.com>
2022-08-23 13:50:11 +0900

    WIP

--- pages/bestRecipe.js
+++ pages/bestRecipe.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import styled from 'styled-components';
 import BestImgDiv from '../components/bestRecipe/BestImgDiv';
 
